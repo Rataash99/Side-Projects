2ecd973f0df669146aa48034bb0bc2bf7df786b9
Rohit <rohit.singh11299@gmail.com>
2022-08-26 21:21:57 +0530

    some changes made

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,23 +1,32 @@
 
-function playSound(e){
-console.log(e)
-    const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key = "${e.keyCode}"]`)    ;
+// function playSound(e){
+// console.log(e)
+//     const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
+//     const key = document.querySelector(`.key[data-key = "${e.keyCode}"]`)    ;
     
-    if(!audio) return;
-    console.log(audio)
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add("playing")
-}
+//     if(!audio) return;
+//     console.log(audio)
+//     audio.currentTime = 0;
+//     audio.play();
+//     key.classList.add("playing")
+// }
 
-function removeTransition(e){
-    if(e.propertyName !== "transform") return;
-    this.classList.remove("playing");
-}
+// function removeTransition(e){
+//     if(e.propertyName !== "transform") return;
+//     this.classList.remove("playing");
+// }
 
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+// const keys = document.querySelectorAll('.key');
+// keys.forEach(key => key.addEventListener("transitionend", removeTransition));
 
-window.addEventListener("keydown",playSound)
+// window.addEventListener("keydown",playSound)
+const data = ['car', 'car', 'truck', 'van', 'walk', 'bike', 'car', 'truck', 'walk', 'van', 'car', 'truck', 'walk', 'van']
 
+const transportation = data.reduce((obj, item) =>{
+    if(!obj[item]){
+        obj[item] = 0;
+    }
+    obj[item]++;
+    return (obj)
+}, {})
+console.log(transportation)
